8747902c5310d61816e78653b230a764572a8ca0
Christian <kalonjic34@gmail.com>
2024-10-17 18:19:27 +0200

    Variables, types, and basic operators in Python

--- 01 - HelloWorld/strings.py
+++ 01 - HelloWorld/strings.py
@@ -4,8 +4,18 @@ print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("Hello"+ " world")
 
-greetings = "Hello"
-name = input("Please enter your name ")
+greeting = "Hello"
+name = "Chris"
 
-print(greetings + name)
-print(greetings +" "+name)
+print(greeting + name)
+print(greeting +" "+name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name +" is "+age+" years old")
+print(type(age))
