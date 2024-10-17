a6b39eaf3ab6907e464f0b7100e73bad7b5a58bd
Christian <kalonjic34@gmail.com>
2024-10-17 17:54:47 +0200

    Printing tabs

---
+++ 01 - HelloWorld/coding_exercise3.py
@@ -0,0 +1,11 @@
+"""
+Coding Exercise 3 - Printing tabs
+Write a program that will produce the following output.
+All the text should appear in your program's output.
+
+Number 1    The Larch
+Number 2    The Horse Chestnut
+"""
+
+print("Number 1\tThe larch")
+print("Number 2\tThe Horse Chestnut")
